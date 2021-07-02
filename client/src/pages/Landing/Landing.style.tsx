a7c239eef7471fe04225a134e0fa40412b33e1e1
Alcatraz9 <rishabhsun.71199@gmail.com>
2021-07-02 20:44:10 +0530

    Revert "static landing page base provided"
    This reverts commit 6b641bb841186900d7662c7dd76a0ceea3daf653.

--- client/src/pages/Landing/Landing.style.tsx
+++ client/src/pages/Landing/Landing.style.tsx
@@ -1,11 +1,8 @@
 import styled from 'styled-components';
 
 export const LandingWrapper = styled.section`
-  min-height: 700px;  
   height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: rgb(12, 1, 75);
-  background-image: radial-gradient(at 50% 100%, rgb(67, 18, 131), rgb(12, 1, 75));
 `;
--- client/src/pages/Landing/Landing.tsx
+++ client/src/pages/Landing/Landing.tsx
@@ -4,23 +4,11 @@ import Layout from 'layout/Layout';
 
 const LandingPage: React.FC<{}> = () => {
   return (
-  <Layout>
-    <LandingWrapper>
-      <div id="home-left">
-        <br />
-        <h1>Let Us Sort the Resumes</h1>
-        <div className="top-subtitle">
-          <h2>
-            Designed by over-enthusiastic engineers, our AI-powered 
-            platform instantly provides you with eligible candidates
-            from your submitted resumes.
-            <br /><br />
-            Save 90% of your time by leaving the tedious task to the machine.
-          </h2>
-        </div>
-      </div>
-    </LandingWrapper>
-  </Layout>
+    <Layout>
+      <LandingWrapper>
+        Landin Page UI
+      </LandingWrapper>
+    </Layout>
   );
-  };
-  export default LandingPage;
+};
+export default LandingPage;
